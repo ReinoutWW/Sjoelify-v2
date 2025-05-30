2bc6d0b7e24b035688651e00c7e0cbddbfa479bc
ReinoutWW <71006243+ReinoutWW@users.noreply.github.com>
2025-05-30 21:16:17 +0200

    Update email

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -770,14 +770,14 @@ export default function Home() {
               <ul className="space-y-2 text-sm">
                 <li>
                   <a 
-                    href="mailto:info@sjoelify.nl" 
+                    href="mailto:info@sjoelify.com" 
                     className="text-gray-400 hover:text-white transition-colors flex items-center gap-2"
                   >
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
                       <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
                       <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
                     </svg>
-                    info@sjoelify.nl
+                    info@sjoelify.com
                   </a>
                 </li>
               </ul>
